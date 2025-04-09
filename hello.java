fc00f5b2ca58655d603a2640c540d597b85c094f
mudavathkalyan <kalyanmudavath238@gmail.com>
2025-04-09 19:01:01 +0530

    java file

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class hello
 {
     public static void main(String[] args) {
-        System.out.println("Hello world  everything is fine");
+        System.out.println("Hello world  everything is fine ok");
     }
 }
